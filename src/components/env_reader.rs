7dd58ae89f833c7b69ccdc3ee85ecb23e09b9ab4
CodeDead <admin@codedead.com>
2026-06-04 14:28:16 +0200

    feat: ask for user input in case env variables are not filled out

--- src/components/env_reader.rs
+++ src/components/env_reader.rs
@@ -15,6 +15,24 @@ impl EnvReader {
         EnvReader {}
     }
 
+    /// Asks the user for input
+    ///
+    /// # Arguments
+    ///
+    /// - `question` - The question to ask the user
+    ///
+    /// # Returns
+    ///
+    /// The user's input as a String
+    pub fn ask_user_input(question: &str) -> String {
+        println!("{}", question);
+        let mut input = String::new();
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+        input.trim().to_string()
+    }
+
     /// Reads the server configuration from environment variables
     ///
     /// # Returns
@@ -32,12 +50,32 @@ impl EnvReader {
             Err(_) => false,
         };
 
-        let copy_indices = match env::var("COPY_INDICES") {
+        let copy_indices: bool = match env::var("COPY_INDICES") {
             Ok(d) => {
                 let res: bool = d.trim().parse().unwrap_or(false);
                 res
             }
-            Err(e) => panic!("COPY_INDICES has not been specified: {}", e),
+            Err(_) => {
+                let mut can_continue = false;
+                let mut res: bool = false;
+                while !can_continue {
+                    let user_response =
+                        Self::ask_user_input(" Would you like to copy collection indices? (y/n):")
+                            .to_lowercase();
+
+                    if user_response == "y" {
+                        res = true;
+                        can_continue = true;
+                    } else if user_response == "n" {
+                        res = false;
+                        can_continue = true;
+                    } else {
+                        println!("Invalid input. Please enter 'y' or 'n'.");
+                    }
+                }
+
+                res
+            }
         };
 
         let read_to_memory = match env::var("READ_TO_MEMORY") {
@@ -45,38 +83,63 @@ impl EnvReader {
                 let res: bool = d.trim().parse().unwrap_or(false);
                 res
             }
-            Err(_) => false,
+            Err(_) => {
+                let mut can_continue = false;
+                let mut res: bool = false;
+                while !can_continue {
+                    let user_response = Self::ask_user_input(
+                        " Would you like to copy collections in-memory and use bulk-insert? (y/n):",
+                    )
+                    .to_lowercase();
+
+                    if user_response == "y" {
+                        res = true;
+                        can_continue = true;
+                    } else if user_response == "n" {
+                        res = false;
+                        can_continue = true;
+                    } else {
+                        println!("Invalid input. Please enter 'y' or 'n'.");
+                    }
+                }
+
+                res
+            }
         };
 
-        let mongodb_origin_connection_string = match env::var("MONGODB_ORIGIN_CONNECTION_STRING") {
-            Ok(d) => d,
-            Err(_) => panic!("MONGODB_ORIGIN_CONNECTION_STRING has not been specified"),
-        };
+        let mongodb_origin_connection_string = env::var("MONGODB_ORIGIN_CONNECTION_STRING")
+            .unwrap_or_else(|_| {
+                Self::ask_user_input(" Please enter the origin MongoDB connection string:")
+            });
 
-        let mongodb_origin_db = match env::var("MONGODB_ORIGIN_DB") {
-            Ok(d) => d,
-            Err(_) => panic!("MONGODB_ORIGIN_DB has not been specified"),
-        };
+        let mongodb_origin_db = env::var("MONGODB_ORIGIN_DB").unwrap_or_else(|_| {
+            Self::ask_user_input(" Please enter the origin MongoDB database name:")
+        });
 
-        let mongodb_origin_collections: Vec<String> = match env::var("MONGODB_ORIGIN_COLLECTIONS") {
+        let mut mongodb_origin_collections: Vec<String> = match env::var(
+            "MONGODB_ORIGIN_COLLECTIONS",
+        ) {
             Ok(d) => d.split(",").map(|s| s.trim().to_string()).collect(),
-            Err(_) => panic!("MONGODB_ORIGIN_COLLECTIONS has not been specified"),
+            Err(_) => {
+                let result = Self::ask_user_input(
+                    " Please enter the origin MongoDB collection names (comma-separated) or leave empty to read all collections:",
+                );
+                if result.is_empty() {
+                    vec![]
+                } else {
+                    result.split(",").map(|s| s.trim().to_string()).collect()
+                }
+            }
         };
-
-        if mongodb_origin_collections.is_empty() {
-            panic!("MONGODB_ORIGIN_COLLECTIONS cannot be empty");
-        }
 
         let mongodb_destination_connection_string =
-            match env::var("MONGODB_DESTINATION_CONNECTION_STRING") {
-                Ok(d) => d,
-                Err(_) => panic!("MONGODB_DESTINATION_CONNECTION_STRING has not been specified"),
-            };
+            env::var("MONGODB_DESTINATION_CONNECTION_STRING").unwrap_or_else(|_| {
+                Self::ask_user_input(" Please enter the destination MongoDB connection string:")
+            });
 
-        let mongodb_destination_db = match env::var("MONGODB_DESTINATION_DB") {
-            Ok(d) => d,
-            Err(_) => panic!("MONGODB_DESTINATION_DB has not been specified"),
-        };
+        let mongodb_destination_db = env::var("MONGODB_DESTINATION_DB").unwrap_or_else(|_| {
+            Self::ask_user_input(" Please enter the destination MongoDB database name:")
+        });
 
         let origin_client = Client::with_uri_str(mongodb_origin_connection_string)
             .await
@@ -85,6 +148,22 @@ impl EnvReader {
         let destination_client = Client::with_uri_str(mongodb_destination_connection_string)
             .await
             .expect("Failed to initialize destination MongoDB client");
+
+        if mongodb_origin_collections.is_empty() {
+            // Read all collections from the origin database
+            let collections = match destination_client
+                .database(&mongodb_destination_db)
+                .list_collection_names()
+                .await
+            {
+                Ok(collections) => collections,
+                Err(e) => panic!("Failed to list collections in origin database: {}", e),
+            };
+
+            for name in collections {
+                mongodb_origin_collections.push(name);
+            }
+        }
 
         AppConfig::new(
             display_header,
